c1b37130a3e1a340a0e82a463622deb91ed24852
Lion <supermylesj@aol.com>
2020-05-02 17:27:57 -0400

    Final

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # All my imports 
-
-from flask import Flask
 import numpy as np
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
+from flask import Flask, jsonify
 
 
 ############################################
@@ -16,12 +16,16 @@ from sqlalchemy import create_engine, func
 engine= create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an exicting database into a new mode;
+Base = automap_base()
 
-Base= automap_base()
+# Reflect the tables 
+Base.prepare(engine, reflect=True)
 
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
+# Create the link to the Database from python 
+session=Session(engine)
 
 
 ############################################
@@ -35,15 +39,114 @@ app = Flask(__name__)
 ############################################
 
 @app.route("/")
-def home():
-    print(" Welcome to the Home Page. <br> All routes listed below")
-    return(
-       f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start> "
-        f"/api/v1.0/<start>/<end>")
+def welcome():
+    return (
+        f"Available Routes:<br>"
+        f"/api/v1.0/precipitation <br>"
+        f"/api/v1.0/stations <br>"
+        f"/api/v1.0/tobs <br>"
+        f"/api/v1.0/start_date <br>"
+        f"/api/v1.0/start_date/end_date <br>"
+        )
+
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+
+
+    session = Session(engine)
+
+    # Query all measurment data
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    measurment_results = []
+    for date, prcp, in results:
+        measurment_dict = {}
+        measurment_dict["date"] = date
+        measurment_dict["prcp"] = prcp
+        measurment_results.append(measurment_dict)
+
+    return jsonify( measurment_results)
+
+@app.route('/api/v1.0/stations')
+def station():
+
+    session = Session(engine)
+
+    # Query all measurment data
+    results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation)
+
+    session.close()
+    # Create a dictionary from the row data and append to a list of all_passengers
+    station_results = []
+    for station, name,latitude,longitude,elevation in results:
+        station_dict = {}
+        station_dict["station"] = station
+        station_dict["name"] = name
+        station_dict["latitude"] = latitude
+        station_dict["longitude"] = longitude
+        station_dict["elevation"] = elevation
+        station_results.append(station_dict)
+
+    return jsonify(station_results)
+
+
+
+@app.route('/api/v1.0/tobs')
+def tobs():
+
+    year_ago= dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    tobs= session.query(Measurement.tobs, Measurement.date).filter(Measurement.date >= year_ago).all()
+    
+    #Close query
+    session.close()
+
+    # Convert list of tuples into normal list
+    temp_list= list(np.ravel(tobs))
 
     
+	
 
+    return jsonify(temp_list)
+
+@app.route('/api/v1.0/<start>')
+def beginning_dates(start):
+    #Set-up for user entry
+    start_date =dt.datetime.strptime(start, '%Y-%m-%d')
+    # Query Min, Avg, Max tempeture and filter by date
+    Date= session.query(func.min (Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs).filter(Measurement.date >= start_date)).all()
+    #Close query
+    session.close()
+    return jsonify(Date)
+
+@app.route('/api/v1.0/<start>/<end>')
+def hawaii(start,end):
+
+     #Set-up for user entry
+    begin_date =dt.datetime.strptime(start, '%Y-%m-%d')
+     #Set-up for user entry
+    end_date =dt.datetime.strptime(end, '%Y-%m-%d')
+
+    # Query Min, Avg, Max tempeture and filter by date
+    summary_date= session.query((func.min (Measurement.tobs), func.avg(Measurement.tobs), func.min (Measurement.tobs).filter( Measurement.date >= begin_date, Measurement.date<=end_date))).all()
+
+    #Close query
+    session.close()
+
+    return jsonify(summary_date)
+
+
+
+
+
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
 
